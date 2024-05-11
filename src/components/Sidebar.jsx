c695a99a93a5e68172d4b5d92a7fa9f6add22127
UHa <https00200@gmail.com>
2024-05-12 00:28:00 +0900

    🚀 [Feat]: Add mainLayout & Sidebar
    - main layout 설정
    - sidebar 위치와 nav 버튼 생성

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import './Sidebar.css'
 
-function Sidebar() {
+export default function Sidebar() {
 	return (
-		<div>Sidebar</div>
+		<div className='sidebar_container'>
+			<div>Calender</div>
+			<nav className='sidebar_nav'>
+				<Link to='/'><button className='sidebar_nav_btn'>ALL</button></Link>
+				<Link to='/diary'><button className='sidebar_nav_btn'>DIARY</button></Link>
+				<Link to='/todo'><button className='sidebar_nav_btn'>TODO</button></Link>
+				<Link to='/culture'><button className='sidebar_nav_btn'>CULTURE</button></Link>
+				<Link to='/memo'><button className='sidebar_nav_btn'>MEMO</button></Link>
+			</nav>
+		</div>
 	)
 }
-
-export default Sidebar
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,17 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import './MainLayout.css'
 
 export default function MainLayout() {
     return (
-        <>
-            <section>
+        <div className='main_container'>
+            <section className='sidebar_nav_btn'>
                 <Sidebar />
             </section>
-            <section>
+            <section className='content_section'>
                 <Header />
                 <Outlet />
             </section>
-        </>
+        </div>
     );
 }
