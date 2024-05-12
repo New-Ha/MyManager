08f33e7e724ae464808c5aaf84edeed74a76c3b4
UHa <https00200@gmail.com>
2024-05-12 17:51:11 +0900

    🚀 [Feat]: Add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import './Header.css';
+import Avatar from '../assets/avatar.svg?react';
 
-function Header() {
-	return (
-		<div>Header</div>
-	)
+export default function Header() {
+    return (
+        <div className="header_container">
+            <div className="select_day">5월 3일</div>
+            <input type="text" className="search_bar" placeholder="검색어를 입력하세요" />
+            <Avatar />
+        </div>
+    );
 }
-
-export default Header
