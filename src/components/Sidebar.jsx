fc921254a0bac4f78ff6deb5dcaffc7325690999
UHa <https00200@gmail.com>
2024-05-28 10:18:26 +0900

    🚀 [Feat]: Navigation Temporary Settings on Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,33 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SidebarContainer = styled.div`
+    width: 5rem;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    border-radius: 50px;
+    background: #f5f5f5;
+    box-shadow: 9px 9px 18px #e4e4e4, -9px -9px 18px #ffffff;
+`;
+
+const NavContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`;
 export default function Sidebar() {
     return (
-        <>
-            <div>🌸</div>
-            <div>
-                <span>📔</span>
-                <span>📚</span>
-                <span>🎞️</span>
-                <span>📋</span>
-            </div>
-            <div>👤</div>
-        </>
+        <SidebarContainer>
+            <Link to="/">🌸</Link>
+            <NavContainer>
+                <Link to="/diary">📔</Link>
+                <Link to="/books">📚</Link>
+                <Link to="/movies">🎞️</Link>
+                <Link to="memos">📋</Link>
+            </NavContainer>
+            <Link to="/profile">👤</Link>
+        </SidebarContainer>
     );
 }
