40812373f7357ecc860f85eb4d5dcbb25fba601a
UHa <https00200@gmail.com>
2024-05-28 10:17:38 +0900

    🚀 [Feat]: Composing elements and styles within the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import Calender from '../components/Calender';
+import TodoList from '../components/TodoList';
+import Weather from '../components/Weather';
+
+const HomePageLayout = styled.div`
+    width: 90vw;
+    display: flex;
+    gap: 30px;
+`;
+
+const RightContainer = styled.div`
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`;
+
 export default function HomePage() {
-	return (
-		<div>HomePage</div>
-	)
+    return (
+        <HomePageLayout>
+            <Calender />
+            <RightContainer>
+                <Weather />
+                <TodoList />
+            </RightContainer>
+        </HomePageLayout>
+    );
 }
