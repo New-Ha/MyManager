c66ecad2492f0d44e2a659fcffc2989462687dde
UHa <https00200@gmail.com>
2024-06-08 20:40:57 +0900

    💄 [Style]: Setting theme & global style
    - Styled-components를 활용해 theme와 global style을 설정
    - App.css, index.css 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './util/theme';
 import Router from './Router';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	
+	body {
+		font-family: 'Noto Sans KR'
+	}
+`;
 
 export default function App() {
     return (
-        <>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
             <Router />
-        </>
+        </ThemeProvider>
     );
 }
---
+++ src/util/theme.js
@@ -0,0 +1,20 @@
+const size = {
+    mobile: '600px',
+    tablet: '900px',
+    computer: '1200px',
+};
+
+const theme = {
+    black: '#4D4D4D',
+		gray: '#D9D9D9',
+    yellow: '#F6BD60',
+		deepGreen: '#51625E',
+    green: '#84A59D',
+    red: '#F28482',
+    purple: '#CCB6DC',
+    mobile: `(max-width: ${size.mobile})`,
+    tablet: `(max-width: ${size.tablet})`,
+    computer: `(max-width: ${size.computer})`,
+};
+
+export default theme;
