ca5acb3a247510f91901174d9135b27d7bbd1769
UHa <https00200@gmail.com>
2024-06-09 01:09:46 +0900

    💄 [Style]: Change Sidebar UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,140 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Home from '../assets/home_icon.svg?react';
+import Calender from '../assets/calender_icon.svg?react';
+import Book from '../assets/book_icon.svg?react';
+import Movie from '../assets/movie_icon.svg?react';
+import Memo from '../assets/memo_icon.svg?react';
+import Logout from '../assets/logout_icon.svg?react';
 
-const SidebarContainer = styled.div`
-    width: 5rem;
-    height: 90vh;
+const SideBarContainer = styled.nav`
+    width: 250px;
+    height: 90%;
+    padding: 4rem 2rem;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
-    border-radius: 50px;
-    background: #f5f5f5;
-    box-shadow: 9px 9px 18px #e4e4e4, -9px -9px 18px #ffffff;
+    justify-content: center;
+    border: 1.5px solid ${props => props.theme.black};
+    border-radius: 20px;
+    gap: 1.25rem;
 `;
 
-const NavContainer = styled.nav`
+const ProfileContainer = styled.div`
+    width: 100%;
+    height: 5rem;
+    border: 1.5px solid ${props => props.theme.black};
+    border-radius: 20px;
+    margin-bottom: 1.75rem;
     display: flex;
-    flex-direction: column;
-    gap: 15px;
+    justify-content: center;
+    align-items: center;
 `;
+
+const AvatarContainer = styled.div`
+    width: 3rem;
+    height: 3rem;
+    border: 1px solid black;
+    border-radius: 50%;
+`;
+
+const UserInfo = styled.div`
+    margin-left: 0.75rem;
+    width: 5.5rem;
+`;
+
+const UserName = styled.p`
+    color: ${props => props.theme.black};
+    font-weight: 700;
+`;
+
+const WelcomeStr = styled.p`
+    color: gray;
+    font-size: 0.75rem;
+    overflow-wrap: wrap;
+`;
+
+const NavButtonContainer = styled.div`
+    border-radius: 10px;
+    :hover {
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    }
+    :active {
+        border: 1.5px solid ${props => props.theme.black};
+        background-color: ${props => props.theme.yellow};
+    }
+`;
+
+const NavLinkButton = styled(Link)`
+    width: 100%;
+    padding: 0.75rem 2rem;
+    border: 1.5px solid white;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+`;
+
+const LogOutContainer = styled(NavLinkButton)`
+    margin-top: 2rem;
+    border: 1.5px solid ${props => props.theme.black};
+    border-radius: 30px;
+    background-color: ${props => props.theme.green};
+`;
+
+const NavStr = styled.div`
+    margin-left: 1.25rem;
+    color: ${props => props.theme.black};
+    font-weight: 500;
+`;
+
+const LogOutStr = styled(NavStr)`
+    color: white;
+`;
+
 export default function Sidebar() {
     return (
-        <SidebarContainer>
-            <Link to="/">🌸</Link>
-            <NavContainer>
-                <Link to="/diary">📔</Link>
-                <Link to="/books">📚</Link>
-                <Link to="/movies">🎞️</Link>
-                <Link to="memos">📋</Link>
-            </NavContainer>
-            <Link to="/profile">👤</Link>
-        </SidebarContainer>
+        <SideBarContainer>
+            <ProfileContainer>
+                <AvatarContainer></AvatarContainer>
+                <UserInfo>
+                    <UserName>유하님💕</UserName>
+                    <WelcomeStr>오늘도 화이팅!</WelcomeStr>
+                </UserInfo>
+            </ProfileContainer>
+            <NavButtonContainer>
+                <NavLinkButton to="/">
+                    <Home />
+                    <NavStr>Home</NavStr>
+                </NavLinkButton>
+            </NavButtonContainer>
+            <NavButtonContainer>
+                <NavLinkButton to="/">
+                    <Calender />
+                    <NavStr>Calender</NavStr>
+                </NavLinkButton>
+            </NavButtonContainer>
+            <NavButtonContainer>
+                <NavLinkButton to="/books">
+                    <Book />
+                    <NavStr>Book</NavStr>
+                </NavLinkButton>
+            </NavButtonContainer>
+            <NavButtonContainer>
+                <NavLinkButton to="/movies">
+                    <Movie />
+                    <NavStr>Movie</NavStr>
+                </NavLinkButton>
+            </NavButtonContainer>
+            <NavButtonContainer>
+                <NavLinkButton to="/memos">
+                    <Memo />
+                    <NavStr>Memo</NavStr>
+                </NavLinkButton>
+            </NavButtonContainer>
+            <LogOutContainer>
+                <Logout />
+                <LogOutStr>Log out</LogOutStr>
+            </LogOutContainer>
+        </SideBarContainer>
     );
 }
