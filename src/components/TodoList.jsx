4e64513fd8facf47a4d78af595f50e7c8987b2d5
UHa <https00200@gmail.com>
2024-06-09 01:57:04 +0900

    💄 [Style]: Change TodoItem style

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,30 @@
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
 
+const mock = [
+    {
+        id: 0,
+        title: '공부하기',
+        isDone: false,
+        content: '리액트 완독하기!!!!',
+        createdAt: '2024-01-01',
+    },
+    {
+        id: 1,
+        title: '청소의날',
+        isDone: true,
+        content: '옷장 대청소하기',
+        createdAt: '2024-01-01',
+    },
+    {
+        id: 2,
+        title: '잠이보약',
+        isDone: false,
+        content: '열심히 꿀잠자기',
+        createdAt: '2024-01-01',
+    },
+];
+
 const TodoListContainer = styled.div`
     padding: 2rem;
     height: 85%;
@@ -10,31 +34,13 @@ const TodoListContainer = styled.div`
     box-shadow: 9px 9px 18px #e4e4e4, -9px -9px 18px #ffffff;
 `;
 
-const mock = [
-	{
-		id: 0,
-		isChecked: false,
-		content: '리액트 공부하기'
-	},
-	{
-		id: 1,
-		isChecked: true,
-		content: '옷장 대청소하기'
-	},
-	{
-		id: 2,
-		isChecked: false,
-		content: '열심히 꿀잠자기'
-	}
-]
 export default function TodoList() {
     return (
         <TodoListContainer>
             Todos
-						{mock.map(item => (
-							<TodoItem id={item.id} isChecked={item.isChecked} content={item.content} />
-						))
-						}
+            {mock.map(item => (
+                <TodoItem key={item.id} id={item.id} title={item.title} isDone={item.isDone} content={item.content} />
+            ))}
         </TodoListContainer>
     );
 }
